a4ba08203f319f43e4bde5df64c9d00dfe8f8b94
LavanyaNethmini <98209905+LavanyaNethmini@users.noreply.github.com>
2025-08-09 10:59:16 +0530

    Update search_service.py

--- api/search_service.py
+++ api/search_service.py
@@ -60,6 +60,6 @@ def search_documents():
 # In[ ]:
 
 
-if __name__ == '__main__':
-    app.run(port=5003)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5003)))
 
