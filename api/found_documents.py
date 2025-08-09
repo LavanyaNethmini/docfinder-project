783e32090a465a9683aa9353dbf7b231029e501e
LavanyaNethmini <98209905+LavanyaNethmini@users.noreply.github.com>
2025-08-09 11:00:34 +0530

    Update found_documents.py

--- api/found_documents.py
+++ api/found_documents.py
@@ -128,6 +128,6 @@ def report_found():
 # In[ ]:
 
 
-if __name__ == '__main__':
-    app.run(port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5002)))
 
