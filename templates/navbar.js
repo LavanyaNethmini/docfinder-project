ab5eed6f720bd1f15ea5764cf8bcdef8ce12f077
LavanyaNethmini <98209905+LavanyaNethmini@users.noreply.github.com>
2025-08-09 16:50:25 +0530

    Delete templates directory

--- templates/navbar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("navbar.html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        });
-});
