784a13f980a771c865db1c524deb95acb99d1e45
LavanyaNethmini <98209905+LavanyaNethmini@users.noreply.github.com>
2025-08-09 13:53:02 +0530

    Update search_service.py

--- api/search_service.py
+++ api/search_service.py
@@ -57,9 +57,4 @@ def search_documents():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 
-# In[ ]:
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5003)))
 
