333c20900a8e6a8666c9fdb0b9511ca750513d39
LavanyaNethmini <98209905+LavanyaNethmini@users.noreply.github.com>
2025-08-09 11:44:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask
+from flask_cors import CORS
+
+from auth_api import users_bp
+from search_service_api import search_bp
+from found_documents_api import documents_bp
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(users_bp)
+app.register_blueprint(search_bp)
+app.register_blueprint(documents_bp)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
