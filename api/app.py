1d73e4fd32a5c1bae9796e338a3dd9cd5fcfa46d
LavanyaNethmini <98209905+LavanyaNethmini@users.noreply.github.com>
2025-08-09 14:13:46 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -25,7 +25,7 @@ app.register_blueprint(documents_bp)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "DocFinder API is running. Use the appropriate /api/ endpoints."})
+    return render_template('Home.html')
 
 if __name__ == "__main__":
     import os
